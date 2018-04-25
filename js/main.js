e53876687ee3d47fc238308c67440d8502e04121
etterwin <etterwin.yll@gmail.com>
2018-04-25 20:13:01 +0300

    update

--- js/main.js
+++ js/main.js
@@ -63,6 +63,38 @@ function toTop() {
     return false;
 }
 
+var menu_selector = "#aside_navigation"; // Переменная должна содержать название класса или идентификатора, обертки нашего меню.
+function onScroll(){
+    var scroll_top = $(document).scrollTop();
+    $(menu_selector + " a").each(function(){
+        var hash = $(this).attr("href");
+        var target = $(hash);
+        if (target.position().top <= scroll_top && target.position().top + target.outerHeight() > scroll_top - 300) {
+            $(menu_selector + " a.aside-nav--active").removeClass("aside-nav--active");
+            $(this).addClass("aside-nav--active");
+        } else {
+            $(this).removeClass("aside-nav--active");
+        }
+    });
+}
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    $("a[href^=#]").click(function(e){
+        e.preventDefault();
+        $(document).off("scroll");
+        $(menu_selector + " a.aside-nav--active").removeClass("aside-nav--active");
+        $(this).addClass("aside-nav--active");
+        var hash = $(this).attr("href");
+        var target = $(hash);
+        $("html, body").animate({
+            scrollTop: target.offset().top
+        }, 500, function(){
+            window.location.hash = hash;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
 /*--------------------------------------------------------------------------------------------------------------------*/
 
 // Стили для кнопки скролла вверх
